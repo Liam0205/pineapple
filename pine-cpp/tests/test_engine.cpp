301d2e0f234d2c25579adf47a8a0120855130791
Liam <liamhuang0205@gmail.com>
2026-05-28 18:53:28 +0800

    bump: v0.9.3

--- pine-cpp/tests/test_engine.cpp
+++ pine-cpp/tests/test_engine.cpp
@@ -9,7 +9,7 @@ using namespace pine;
 namespace {
 
 constexpr const char* kCopyConfig = R"({
-  "_PINEAPPLE_VERSION": "0.9.2",
+  "_PINEAPPLE_VERSION": "0.9.3",
   "pipeline_config": {
     "operators": {
       "copy": {
--- pine-cpp/tests/test_panic.cpp
+++ pine-cpp/tests/test_panic.cpp
@@ -24,7 +24,7 @@ TEST_CASE("PanicError: is a pine::Error") {
 namespace {
 
 constexpr const char* kBadCopyConfig = R"({
-  "_PINEAPPLE_VERSION": "0.9.2",
+  "_PINEAPPLE_VERSION": "0.9.3",
   "pipeline_config": {
     "operators": {
       "bad": {
