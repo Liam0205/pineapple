a8720d9f101fa2f56c25642073bed875446067fd
Liam <liamhuang0205@gmail.com>
2026-05-31 23:18:02 +0800

    bump: v0.9.7

--- pine-cpp/tests/test_config.cpp
+++ pine-cpp/tests/test_config.cpp
@@ -9,7 +9,7 @@ using namespace pine;
 namespace {
 
 constexpr const char* kSimpleConfig = R"({
-  "_PINEAPPLE_VERSION": "0.9.6",
+  "_PINEAPPLE_VERSION": "0.9.7",
   "pipeline_config": {
     "operators": {
       "first": {
--- pine-cpp/tests/test_engine.cpp
+++ pine-cpp/tests/test_engine.cpp
@@ -9,7 +9,7 @@ using namespace pine;
 namespace {
 
 constexpr const char* kCopyConfig = R"({
-  "_PINEAPPLE_VERSION": "0.9.6",
+  "_PINEAPPLE_VERSION": "0.9.7",
   "pipeline_config": {
     "operators": {
       "copy": {
