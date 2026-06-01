8eb45dc7ffb9c2573a5ef1f4591d8f310ca2f8a0
Liam <liamhuang0205@gmail.com>
2026-06-01 15:29:25 +0800

    bump: v0.9.8

--- pine-cpp/tests/test_engine.cpp
+++ pine-cpp/tests/test_engine.cpp
@@ -9,7 +9,7 @@ using namespace pine;
 namespace {
 
 constexpr const char* kCopyConfig = R"({
-  "_PINEAPPLE_VERSION": "0.9.7",
+  "_PINEAPPLE_VERSION": "0.9.8",
   "pipeline_config": {
     "operators": {
       "copy": {
