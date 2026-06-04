ad9e142d8fe691c34b859232d74e17512af79509
Liam <liamhuang0205@gmail.com>
2026-06-04 22:25:07 +0800

    bump: v0.9.13

--- pine-cpp/tests/test_trace.cpp
+++ pine-cpp/tests/test_trace.cpp
@@ -9,7 +9,7 @@ using namespace pine;
 namespace {
 
 constexpr const char* kBaseConfigNoDebug = R"({
-  "_PINEAPPLE_VERSION": "0.9.12",
+  "_PINEAPPLE_VERSION": "0.9.13",
   "pipeline_config": {
     "operators": {
       "op": {
