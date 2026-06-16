31c9acb7387d43e47d2b2fafe862f6c75aa7124d
Liam <liamhuang0205@gmail.com>
2026-06-16 20:04:36 +0800

    chore: bump version 0.10.5 → 0.10.6

--- pine-cpp/tests/test_trace.cpp
+++ pine-cpp/tests/test_trace.cpp
@@ -9,7 +9,7 @@ using namespace pine;
 namespace {
 
 constexpr const char* kBaseConfigNoDebug = R"({
-  "_PINEAPPLE_VERSION": "0.10.5",
+  "_PINEAPPLE_VERSION": "0.10.6",
   "pipeline_config": {
     "operators": {
       "op": {
