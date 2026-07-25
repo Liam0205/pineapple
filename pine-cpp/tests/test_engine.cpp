ca2baf078f7eafa73d8794ec81822b00ed41a1a4
Liam <liamhuang0205@gmail.com>
2026-07-25 12:41:51 +0800

    bump: v0.10.16

--- pine-cpp/tests/test_engine.cpp
+++ pine-cpp/tests/test_engine.cpp
@@ -13,7 +13,7 @@ using namespace pine;
 namespace {
 
 constexpr const char* kCopyConfig = R"({
-  "_PINEAPPLE_VERSION": "0.10.15",
+  "_PINEAPPLE_VERSION": "0.10.16",
   "pipeline_config": {
     "operators": {
       "copy": {
--- pine-cpp/tests/test_parallel.cpp
+++ pine-cpp/tests/test_parallel.cpp
@@ -7,7 +7,7 @@ using namespace pine;
 namespace {
 
 constexpr const char* kParallelConfig = R"({
-  "_PINEAPPLE_VERSION": "0.10.15",
+  "_PINEAPPLE_VERSION": "0.10.16",
   "pipeline_config": {
     "operators": {
       "copy_tag": {
